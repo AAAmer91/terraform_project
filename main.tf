cc2695158f5526dbf647459fa7ae4f7ba63df513
Ahmed A. Amer <a.a.amer91@gmail.com>
2024-09-28 00:52:18 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ variable "ssh_public_key" {
 
 # Create a security group to allow SSH and HTTP
 resource "aws_security_group" "allow_ssh_http" {
-  name        = "allow_ssh_http"
+  name        = "allow_ssh_http_new"
   
   ingress {
     from_port   = 22  # Allow SSH
