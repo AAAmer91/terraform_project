ac58ef4248eb40df593aed0bcf32bc44e5463fd7
Ahmed A. Amer <a.a.amer91@gmail.com>
2024-09-29 10:54:36 +0300

    Airflow Addition

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-# Provider block - specifies you're using Amazon Web Servers
+# Provider block - specifies you're using AWS
 provider "aws" {
   region = "us-west-2"  # Replace with your preferred AWS region
 }
 
 # Create a key pair to access the EC2 instance (replace with your preferred key name)
 resource "aws_key_pair" "terraform_key" {
-  key_name   = "terraform_key2"
+  key_name   = "terraform_key"
   public_key = var.ssh_public_key
 }
 
@@ -14,8 +14,8 @@ variable "ssh_public_key" {
 }
 
 # Create a security group to allow SSH and HTTP
-resource "aws_security_group" "allow_ssh_http" {
-  name        = "allow_ssh_http2"
+resource "aws_security_group" "allow_ssh_http_airflow" {
+  name        = "allow_ssh_http_airflow"
   
   ingress {
     from_port   = 22  # Allow SSH
@@ -25,8 +25,15 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 
   ingress {
-    from_port   = 80  # Allow HTTP
+    from_port   = 80  # Allow HTTP (Airflow Web UI will be bound to port 80)
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080  # Allow Airflow on port 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -39,22 +46,50 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 }
 
-# Create an EC2 instance
+# Data block to find any existing EC2 instance
+data "aws_instance" "existing_instance" {
+  filter {
+    name   = "tag:Name"
+    values = ["MyTerraformEC2Instance"]
+  }
+}
+
+# Use an existing EC2 instance or create a new one
 resource "aws_instance" "my_ec2_instance" {
+  count = length(data.aws_instance.existing_instance.ids) == 0 ? 1 : 0
+
   ami           = "ami-05134c8ef96964280"  # Replace with a valid AMI ID for your region
   instance_type = "t2.micro"
 
   # Associate the key pair and security group
   key_name      = aws_key_pair.terraform_key.key_name
-  security_groups = [aws_security_group.allow_ssh_http.name]
+  security_groups = [aws_security_group.allow_ssh_http_airflow.name]
 
-  # Tags for better management
+  # User data script to install Apache Airflow
+  user_data = <<-EOF
+    #!/bin/bash
+    # Update the instance and install dependencies
+    sudo apt update -y
+    sudo apt upgrade -y
+    sudo apt install -y python3-pip
+
+    # Install Apache Airflow
+    pip3 install apache-airflow
+
+    # Initialize the Airflow database
+    airflow db init
+
+    # Start Airflow Webserver and Scheduler
+    nohup airflow webserver --port 8080 &  # Starts webserver on port 8080
+    nohup airflow scheduler &  # Starts the scheduler
+  EOF
+
   tags = {
     Name = "MyTerraformEC2Instance"
   }
 }
 
-# Output the public IP of the EC2 instance
+# Output the public IP of the EC2 instance (whether reused or newly created)
 output "ec2_public_ip" {
-  value = aws_instance.my_ec2_instance.public_ip
+  value = length(data.aws_instance.existing_instance.ids) > 0 ? data.aws_instance.existing_instance.public_ip : aws_instance.my_ec2_instance.public_ip
 }
